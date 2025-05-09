61097487a3b32d71a8506e7899ad0b3b29115fe0
MohamedBatari <mohamed.ari1997@gmail.com>
2025-05-09 14:37:03 +0400

    your message

---
+++ PTfinder.API/DATA/DTO/ReviewCreateDto.cs
@@ -0,0 +1,10 @@
+﻿namespace PTfinder.API.DATA.DTO
+{
+    public class ReviewCreateDto
+    {
+        public string StudentName { get; set; }
+        public string Comment { get; set; }
+        public int Rating { get; set; }
+        public int CoachId { get; set; }
+    }
+}
---
+++ PTfinder.API/DATA/DTO/ReviewDto.cs
@@ -0,0 +1,10 @@
+﻿namespace PTfinder.API.DATA.DTO
+{
+    public class ReviewDto
+    {
+        public int Id { get; set; }
+        public string StudentName { get; set; }
+        public string Comment { get; set; }
+        public int Rating { get; set; }
+    }
+}
