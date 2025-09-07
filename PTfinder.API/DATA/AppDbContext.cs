fb999211b104e8005b11b730a9136d6168cbc116
MohamedBatari <mohamed.ari1997@gmail.com>
2025-09-07 14:05:46 +0400

    Your commit message

--- PTfinder.API/DATA/AppDbContext.cs
+++ PTfinder.API/DATA/AppDbContext.cs
@@ -1,5 +1,4 @@
-﻿using System.Linq;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using PTfinder.API.DATA.Modules;
 
 namespace PTfinder.API.DATA
@@ -19,63 +18,80 @@ namespace PTfinder.API.DATA
         public DbSet<City> Cities { get; set; } = null!;
         public DbSet<Area> Areas { get; set; } = null!;
         public DbSet<GalleryMedia> GalleryMedia { get; set; } = null!;
-
         public DbSet<EmailVerification> EmailVerifications { get; set; } = null!;
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            // Relationships you explicitly want
-            modelBuilder.Entity<Category>()
-                .HasMany(c => c.Specialities)
-                .WithOne(s => s.Category)
-                .OnDelete(DeleteBehavior.Cascade);
-
-            modelBuilder.Entity<Category>()
-                .HasMany(c => c.Coaches)
-                .WithOne(c => c.Category)
-                .OnDelete(DeleteBehavior.Restrict);
-
-            modelBuilder.Entity<Speciality>()
-                .HasMany(s => s.Coaches)
-                .WithOne(c => c.Speciality)
-                .OnDelete(DeleteBehavior.Restrict);
-
+            // Safe cascades for the location hierarchy
             modelBuilder.Entity<Country>()
                 .HasMany(c => c.Cities)
                 .WithOne(c => c.Country)
+                .HasForeignKey(c => c.CountryId)
                 .OnDelete(DeleteBehavior.Cascade);
 
             modelBuilder.Entity<City>()
                 .HasMany(c => c.Areas)
                 .WithOne(a => a.City)
+                .HasForeignKey(a => a.CityId)
                 .OnDelete(DeleteBehavior.Cascade);
 
+            // Category ↔ Speciality and Category ↔ Coach
+            modelBuilder.Entity<Category>()
+                .HasMany(c => c.Specialities)
+                .WithOne(s => s.Category)
+                .HasForeignKey(s => s.CategoryId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Category>()
+                .HasMany(c => c.Coaches)
+                .WithOne(c => c.Category)
+                .HasForeignKey(c => c.CategoryId)
+                .OnDelete(DeleteBehavior.NoAction); // break cascades here
+
+            modelBuilder.Entity<Speciality>()
+                .HasMany(s => s.Coaches)
+                .WithOne(c => c.Speciality)
+                .HasForeignKey(c => c.SpecialityId)
+                .OnDelete(DeleteBehavior.NoAction);   // break cascades here
+
+            // Coach → Country/City/Area must NOT cascade
+            modelBuilder.Entity<Coach>()
+                .HasOne(c => c.Country)
+                .WithMany()
+                .HasForeignKey(c => c.CountryId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            modelBuilder.Entity<Coach>()
+                .HasOne(c => c.City)
+                .WithMany()
+                .HasForeignKey(c => c.CityId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            modelBuilder.Entity<Coach>()
+                .HasOne(c => c.Area)
+                .WithMany()
+                .HasForeignKey(c => c.AreaId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            // Price precision
             modelBuilder.Entity<Coach>()
                 .Property(c => c.Price)
                 .HasPrecision(10, 2);
 
-            // EmailVerification: lengths + indexes so SQL Server can index
+            // EmailVerification indexes
             modelBuilder.Entity<EmailVerification>(b =>
             {
                 b.Property(x => x.Email).IsRequired().HasMaxLength(320);
                 b.Property(x => x.Token).IsRequired().HasMaxLength(200);
-
                 b.HasIndex(x => x.Email);
                 b.HasIndex(x => x.Token).IsUnique();
             });
 
             base.OnModelCreating(modelBuilder);
 
-            // IMPORTANT: Do NOT blanket-force Restrict on all FKs here,
-            // or you’ll override the cascades defined above.
-            // If you *really* want a default, you can selectively adjust only ClientCascade:
-            //
-            // foreach (var fk in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
-            // {
-            //     if (fk.DeleteBehavior == DeleteBehavior.ClientCascade)
-            //         fk.DeleteBehavior = DeleteBehavior.Restrict;
-            // }
+            // DO NOT add any global override loop here that changes all DeleteBehavior.
         }
     }
 }
+
 
