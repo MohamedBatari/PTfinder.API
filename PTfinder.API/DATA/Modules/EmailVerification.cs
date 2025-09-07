034cc414328821157325d63e044d7046f6e7cd02
MohamedBatari <mohamed.ari1997@gmail.com>
2025-09-07 05:20:06 +0400

    Add EmailVerification entity and configure indexes

---
+++ PTfinder.API/DATA/Modules/EmailVerification.cs
@@ -0,0 +1,21 @@
+﻿// DATA/Modules/EmailVerification.cs
+using System.ComponentModel.DataAnnotations;
+
+namespace PTfinder.API.DATA.Modules
+{
+    public class EmailVerification
+    {
+        public int Id { get; set; }
+
+        [Required, MaxLength(256)]
+        public string Email { get; set; } = null!; // normalized lowercased
+
+        [Required, MaxLength(200)]
+        public string Token { get; set; } = null!; // url-safe token
+
+        public DateTime ExpiresUtc { get; set; }
+
+        public DateTime? UsedAtUtc { get; set; } // null => not used yet
+    }
+}
+
---
+++ PTfinder.API/Migrations/20250906233406_AddEmailVerificationToCoach.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PTfinder.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEmailVerificationToCoach : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EmailVerificationExpiresUtc",
+                table: "Coaches",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "EmailVerificationToken",
+                table: "Coaches",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "EmailVerified",
+                table: "Coaches",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EmailVerificationExpiresUtc",
+                table: "Coaches");
+
+            migrationBuilder.DropColumn(
+                name: "EmailVerificationToken",
+                table: "Coaches");
+
+            migrationBuilder.DropColumn(
+                name: "EmailVerified",
+                table: "Coaches");
+        }
+    }
+}
